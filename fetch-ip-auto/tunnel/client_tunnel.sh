52df9cd90d2f04a67948a92fab7e99812f75f825
δDD <64313188+DDjivan@users.noreply.github.com>
2025-05-27 13:33:04 +0200

    Update client_tunnel.sh

--- fetch-ip-auto/tunnel/client_tunnel.sh
+++ fetch-ip-auto/tunnel/client_tunnel.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+exec > /tmp/client_tunnel.log 2>&1
+echo "Starting client_tunnel.sh at $(date)"
 
 if [[ $(uname -n) == *"raspberrypi"* ]]; then
-    source ~/LRSVZZ-2025/fetch-ip-auto/tunnel/client_tunnelconfig.cfg
+    source /home/nous/LRSVZZ-2025/fetch-ip-auto/tunnel/client_tunnelconfig.cfg
 else
     source ./client_tunnelconfig.cfg
 fi
