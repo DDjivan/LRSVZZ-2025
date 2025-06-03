12594593602b1200d629705890f40de6eaa3052e
δDD <64313188+DDjivan@users.noreply.github.com>
2025-06-03 15:17:12 +0200

    Added code from server_ip-getter-displayer.py

--- second-python/app.py
+++ second-python/app.py
@@ -84,6 +84,44 @@ def web_markdown(filename:str) :
 
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- #
 
+from flask import request
+from datetime import datetime
+
+sFile = '../fetch-ip/rpi4_ip.txt'
+
+@app.route('/update_ip', methods=['POST'])
+def update_ip() :
+    hostname   = request.form['hostname']
+    username   = request.form['username']
+    ip_address = request.form['ip']
+
+    current_time = datetime.now().isoformat().replace('T', ' ')  # ISO 8601 but remove the T
+
+    # write datetime, IP address, hostname, and username in a file
+    with open(sFile, 'a') as f :
+        f.write(f'{current_time} - {ip_address} - {hostname} - {username}\n')
+
+    return 'RPi2-Listener here. Well received the three of your data!', 200
+
+@app.route('/view_ips', methods=['GET'])
+def view_ips() :
+    try :
+        with open(sFile, 'r') as f :
+            content = f.read()
+
+        # return f'<pre>{content}</pre>'  
+        content = f'<pre>{content}</pre>'  
+
+    except FileNotFoundError :
+        # return 'The file is empy: no IP addresses recorded yet.', 404
+        content = "The file is empy: no IP addresses recorded yet."
+
+    finally: 
+        new_content = md_to_html(content)
+        return render_template('index.html', content=new_content)
+
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- #
+
 from subprocess import run
 from datetime import datetime
 # from json import load
