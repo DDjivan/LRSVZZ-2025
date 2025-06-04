bc7e262b5bb2b9bcf114e08598e6b1b1c3b50785
δDD <64313188+DDjivan@users.noreply.github.com>
2025-06-04 11:12:19 +0200

    Update client_tunnel02.sh

--- fetch-ip-auto/tunnel/client_tunnel02.sh
+++ fetch-ip-auto/tunnel/client_tunnel02.sh
@@ -8,7 +8,9 @@ else
     CONFIG_PATH="client_tunnel02config.json"
 fi
 
-if ! jq -e ".\"$HOSTNAME\"" "$CONFIG_PATH" > /dev/null; then
+if jq -e ".\"$HOSTNAME\"" "$CONFIG_PATH" > /dev/null; then
+  echo "FOUND: Tunnel configuration for hostname: $HOSTNAME"
+else
   echo "No tunnel configuration found for hostname: $HOSTNAME"
   echo "Using default configuration."
   HOSTNAME="default"
@@ -21,9 +23,10 @@ LOCAL_PORT=$(jq -r ".\"$HOSTNAME\".PORT_LOCAL" "$CONFIG_PATH")
 REMOTE_PORT=$(jq -r ".\"$HOSTNAME\".PORT_REMOTE" "$CONFIG_PATH")
 
 sleep 1
+echo ""
 
 while true; do
-    echo ssh -N -R $REMOTE_PORT:localhost:$LOCAL_PORT $USERNAME@$ADDRESSEIP
+    echo $ ssh -N -R $REMOTE_PORT:localhost:$LOCAL_PORT $USERNAME@$ADDRESSEIP
     ssh -N -R $REMOTE_PORT:localhost:$LOCAL_PORT $USERNAME@$ADDRESSEIP
     sleep 10
 done
