170c4af16e7681cb923a01e880dbb3ec4b7b6053
δDD <64313188+DDjivan@users.noreply.github.com>
2025-06-05 10:58:26 +0200

    Update client_tunnel02.sh

--- fetch-ip-auto/tunnel/client_tunnel02.sh
+++ fetch-ip-auto/tunnel/client_tunnel02.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 
 HOSTNAME=$(hostname)
+name_CONFIG="client_tunnel02config.json"
 
-if [[ HOSTNAME == "raspberrypi-client"* ]]; then
-    CONFIG_PATH="/home/nous/LRSVZZ-2025/fetch-ip-auto/tunnel/client_tunnel02config.json"
+if [[ "$HOSTNAME" == "raspberrypi-client"* ]]; then
+    path_BASE="/home/nous/LRSVZZ-2025/fetch-ip-auto/tunnel"
+    path_CONFIG="$path_BASE/$name_CONFIG"
 else
-    CONFIG_PATH="client_tunnel02config.json"
+    path_CONFIG="$name_CONFIG"
 fi
 
-if jq -e ".\"$HOSTNAME\"" "$CONFIG_PATH" > /dev/null; then
-  echo "FOUND: Tunnel configuration for hostname: $HOSTNAME"
+if [[ ! -f "$path_CONFIG" ]]; then
+    echo "ERROR: Configuration file not found: $path_CONFIG"
+    exit 1
 else
-  echo "No tunnel configuration found for hostname: $HOSTNAME"
-  echo "Using default configuration."
+    echo "INFO: Found configuration file: $path_CONFIG"
+fi
+
+if jq -e ".\"$HOSTNAME\"" "$path_CONFIG" > /dev/null; then
+  echo "INFO: Found tunnel configuration for hostname: $HOSTNAME"
+else
+  echo "WARN: No tunnel configuration found for hostname: $HOSTNAME"
+  echo "INFO: Using default configuration."
   HOSTNAME="default"
 fi
 
-USERNAME=$(jq -r '.server_USERNAME' "$CONFIG_PATH")
-ADDRESSEIP=$(jq -r '.server_IP' "$CONFIG_PATH")
+USERNAME=$(jq -r '.server_USERNAME' "$path_CONFIG")
+ADDRESSEIP=$(jq -r '.server_IP' "$path_CONFIG")
 
-LOCAL_PORT=$(jq -r ".\"$HOSTNAME\".PORT_LOCAL" "$CONFIG_PATH")
-REMOTE_PORT=$(jq -r ".\"$HOSTNAME\".PORT_REMOTE" "$CONFIG_PATH")
+LOCAL_PORT=$(jq -r ".\"$HOSTNAME\".PORT_LOCAL" "$path_CONFIG")
+REMOTE_PORT=$(jq -r ".\"$HOSTNAME\".PORT_REMOTE" "$path_CONFIG")
 
 sleep 1
 echo ""
 
 while true; do
-    echo $ ssh -N -R $REMOTE_PORT:localhost:$LOCAL_PORT $USERNAME@$ADDRESSEIP
+    echo "INFO: Running $ ssh -N -R $REMOTE_PORT:localhost:$LOCAL_PORT $USERNAME@$ADDRESSEIP"
     ssh -N -R $REMOTE_PORT:localhost:$LOCAL_PORT $USERNAME@$ADDRESSEIP
     sleep 10
 done
