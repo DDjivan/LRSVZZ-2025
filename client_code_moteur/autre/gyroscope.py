968a84de45e0ee388fd24a85ac9ab5a0b4283c39
BenYade <benyade9000@hotmail.com>
2025-06-24 10:00:47 +0200

    Update gyroscope.py

--- client_code_moteur/autre/gyroscope.py
+++ client_code_moteur/autre/gyroscope.py
@@ -1,30 +1,23 @@
 import time
-import board              
-import busio            
+import board
+import busio
 import adafruit_lsm9ds0
-from math import *
+import math  # Importer le module math pour atan2
 
 i2c = busio.I2C(board.SCL, board.SDA)
-#print("Creation de l'interface I2C effectuee") 
-
 sensor = adafruit_lsm9ds0.LSM9DS0_I2C(i2c)
-#print("Initialisation du gyroscope effectuee")
-
 
 while True:
     accel_x, accel_y, accel_z = sensor.acceleration
     gyro_x, gyro_y, gyro_z = sensor.gyro
     mag_x, mag_y, mag_z = sensor.magnetic
 
-    heading = atan2(mag_y, mag_x);
-    heading *= 180.0/3.14
-    if (heading<0) :
-        heading+= 360.0
+    # Calculer l'angle par rapport au nord
+    angle_radians = math.atan2(mag_y, mag_x)
+    angle_degrees = math.degrees(angle_radians)  # Convertir en degrés
+    angle_degrees = angle_degrees + 360 if angle_degrees < 0 else angle_degrees  # Ajuster pour obtenir un angle positif
 
-    #print("Gyroscope (degre/s): X={:.2f} Y={:.2f} Z={:.2f}".format(gyro_x, gyro_y, gyro_z))
-    #print("Accelerometre (m/s2): Z={:.2f} Y={:.2f} Z={:.2f}".format(accel_x, accel_y, accel_z))
+    print("Angle par rapport au nord (degrés): {:.2f}".format(angle_degrees))
     print("Magnetometre (uT): X={:.2f} Y={:.2f} Z={:.2f}".format(mag_x, mag_y, mag_z))
-    print("VLVLVLLMBLMBA.PK.R3OLRMO valeur en degres : ", heading)
     print("----------------------------------------------------------------------")
     time.sleep(2.0)
-
