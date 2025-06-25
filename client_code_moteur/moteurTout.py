10245669edbac1c4886a9714fc625ae8ddfce8f6
BenYade <benyade9000@hotmail.com>
2025-06-25 21:21:10 +0200

    Update moteurTout.py

--- client_code_moteur/moteurTout.py
+++ client_code_moteur/moteurTout.py
@@ -111,6 +111,7 @@ class Robot:
             mot.start_pin(self.pi, self.gpioM1, -1)
             mot.start_pin(self.pi, self.gpioM2, -1)
             while not(aFinal-10<a0 and a0<aFinal+10):
+                print("testsets")
                 time.sleep(0.1)
             self.direction_index = (self.direction_index + 1) % 4  # Tourner à droite
         else:
@@ -118,6 +119,7 @@ class Robot:
             mot.start_pin(self.pi, self.gpioM1, 1)
             mot.start_pin(self.pi, self.gpioM2, 1)
             while not(aFinal-10<a0 and a0<aFinal+10):
+                print("testetszaeaeazea")
                 time.sleep(0.1)
             self.direction_index = (self.direction_index - 1) % 4  # Tourner à gauche
         self.stopMoteurs()
