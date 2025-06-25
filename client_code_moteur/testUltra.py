20c55cc301ed1dd4f4aa81d25bcef31d75219d19
BenYade <benyade9000@hotmail.com>
2025-06-25 17:24:44 +0200

    Update testUltra.py

--- client_code_moteur/testUltra.py
+++ client_code_moteur/testUltra.py
@@ -4,9 +4,9 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(24, GPIO.IN)
 
-print("Lecture de l'état de ECHO pendant 5 secondes :")
+print("Lecture de l'état de ECHO pendant 10 secondes :")
 start = time.time()
-while time.time() - start < 5:
+while time.time() - start < 10:
     print(GPIO.input(24))
     time.sleep(0.1)
 
